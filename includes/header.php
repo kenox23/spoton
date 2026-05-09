97f9c6b0f59b465e0422d2a5818f1aec59091881
Jamuel Halili Bacus <bacusjamuel96@gmail.com>
2026-05-09 15:39:20 +0800

    added logospoton.png on images, changed header.php to display logospoton.png instead of logosrp.png

--- includes/header.php
+++ includes/header.php
@@ -20,7 +20,7 @@
   	<div class = "container">
   		<nav class="navbar navbar-expand-lg navbar-light bg-light">
   	<a class="navbar-brand" href="index.php">
-  		<img src="images/logosrp.png" width="50" height="40">
+  		<img src="images/logospoton.png" width="50" height="40">
   	</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
