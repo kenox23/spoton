5db7583b90eda0301a1d4017e13fc1551b991578
Joseph Jimenez <jayminezthegreat18@gmail.com>
2026-05-09 18:31:31 +0800

    Update readrecords.php

--- readrecords.php
+++ readrecords.php
@@ -1,15 +1,16 @@
 <?php
-include 'connect.php';
-
-if (!$connection) {
-    die('Could not connect: ' . mysqli_connect_error());
-}
-
-// Query to get student information from both tables
-$query = 'SELECT * FROM tbluser, tblstudent WHERE tbluser.userid = tblstudent.studentid';
-$resultset = mysqli_query($connection, $query);
-
-if (!$resultset) {
-    die('Query failed: ' . mysqli_error($connection));
-}
+	
+	include 'connect.php';
+	
+	if (!$connection) {
+	    die('Could not connect: ' . mysqli_connect_error());
+	}
+	
+	// Query to get ALL student data including program and yearlevel
+	$query = 'SELECT * FROM tbluser, tblstudent WHERE tbluser.userid = tblstudent.studentid';
+	$resultset = mysqli_query($connection, $query);
+	
+	if(!$resultset) {
+	    die('Query failed: ' . mysqli_error($connection));
+	}
 ?>
