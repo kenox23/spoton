09a7288c3b05908d9fd3ba5f675f1460e469cb95
Joseph Jimenez <jayminezthegreat18@gmail.com>
2026-05-09 18:29:51 +0800

    Update readrecords.php

--- readrecords.php
+++ readrecords.php
@@ -1,12 +1,15 @@
 <?php
 include 'connect.php';
 
-// Query to get student information from both tables without JOIN
-$query = "SELECT * FROM tbluser, tblstudent WHERE tbluser.userid = tblstudent.studentid";
+if (!$connection) {
+    die('Could not connect: ' . mysqli_connect_error());
+}
+
+// Query to get student information from both tables
+$query = 'SELECT * FROM tbluser, tblstudent WHERE tbluser.userid = tblstudent.studentid';
 $resultset = mysqli_query($connection, $query);
 
-// Check if query was successful
 if (!$resultset) {
-    die("Query failed: " . mysqli_error($connection));
+    die('Query failed: ' . mysqli_error($connection));
 }
 ?>
