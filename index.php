2690c392c90d7e3f613d99933c20a2312888c5df
Jamuel Halili Bacus <bacusjamuel96@gmail.com>
2026-05-09 15:53:14 +0800

    changed text INDEX PAGE to just red line

--- index.php
+++ index.php
@@ -3,15 +3,11 @@
     require_once 'includes/header.php'; 
 ?>
 
-<div style='background-color:#ffff00'>
-    <center>
-        <p style="color:white"><h2>INDEX PAGE</h2></p>
-    </center>
-</div>    
+<div style="background-color:#8a252c; height:10px; width:100%;"></div>
 
 <div>
 	<button><a href="register.php">REGISTER NEW USER</a></button><br>
 	<button><a href="login.php">LOGIN</a></button>
 </div>
 
-<?php require_once 'includes/footer.php'; ?>
+<?php require_once 'includes/footer.php'; ?>    
